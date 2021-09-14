5019ad747a6de07524b93dd5172a85b48d7adc9f
User1308 <raphaelme1308@gmail.com>
2021-09-14 20:56:34 +0100

    Initial models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    author = models.CharField(max_length=200)
+    uploaded_by = models.ForeignKey('User', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    cover = models.ImageField(upload_to='images/')
+    category = models.ForeignKey('Category',null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} by {self.author}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class Chapter(models.Model):
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    completed = models.BooleanField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class User(AbstractUser):
+    email = models.EmailField(_('Email Address'), unique=True)
+
+    def __str__(self):
+        return self.username
+
+class PreferredCategories(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
